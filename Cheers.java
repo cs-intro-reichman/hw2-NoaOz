cca712f8899db7ab72d861bf0c591478c6fe94a7
NoaOz <noa.oz01@post.runi.ac.il>
2024-11-20 19:47:03 +0200

    completed Cheers

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,44 @@
-// Prints a crowd cheering output.
+
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	 String cheers = args[0];
+         int Number = Integer.parseInt(args[1]);
+         String specialletters = "AEFHILMNOR";
+         String Uppercasecheers = "";
+         String a = "";
+
+         for (int i = 0; i < cheers.length(); i++) {
+                char ch = cheers.charAt(i);
+                if (ch >= 'a' && ch <= 'z') {
+                    ch = (char) (ch - ('a' - 'A'));
+                    Uppercasecheers = Uppercasecheers + ch ; 
+                }
+                else {
+                        Uppercasecheers = Uppercasecheers + ch ;
+                }
         }
-}
+
+         for (int i = 0; i < Uppercasecheers.length(); i++) {
+                char ch = Uppercasecheers.charAt(i);
+                for (int j = 0; j < specialletters.length(); j++){
+                        if(specialletters.indexOf(ch) == -1){
+                              a = "a";       
+                        }
+                         else {
+                               a = "an";
+                                }
+                        }
+                        System.out.println("Give me " + a + " " + ch + ": " + ch + "!" );     
+                }
+                System.out.println("What does that spell?");
+                int i = 0;
+                while (i < Number){
+                        System.out.println(Uppercasecheers + "!!!");
+                        i++ ;
+                }
+                
+         }
+
+
+        }
+
